3e64814c884555a1d99d94bef600aca3d408b673
Ishank Mishra <ishankmishra19@gmail.com>
2024-08-14 01:30:22 +0530

    pausemenu implementation

--- MyGame/scenes/House/Downstairs.gd
+++ MyGame/scenes/House/Downstairs.gd
@@ -53,7 +53,3 @@ func _on_transition_animation_finished(anim_name):
 	if anim_name == "bye":
 		get_tree().change_scene_to_file("res://scenes/House/Lobby.tscn")
 
-
-func _process(_delta):
-	$PauseMenu.show_menu()
-	
--- MyGame/scenes/pauseMenu/pauseMenu.gd
+++ MyGame/scenes/pauseMenu/pauseMenu.gd
@@ -1,18 +1,20 @@
 extends Node2D
-@onready var incoming = get_node("res://scenes/pauseMenu/input_handle")
 
 func _ready():
+	set_process_input(PROCESS_MODE_ALWAYS)
 	$CanvasLayer.visible = false
-	incoming.connect("input_received", _on_input_received())
 
-
-func _on_input_received():
-	print("HI")
 func show_menu():
 	get_tree().paused = true
 	$CanvasLayer.visible = true
-	
 
 func hide_menu():
 	get_tree().paused = false
-	$CanvasLayer.visible = true
+	$CanvasLayer.visible = false
+
+func _input(event):
+	if event.is_action_pressed("ui_cancel") and not get_tree().paused:
+		show_menu()
+	elif event.is_action_pressed("ui_cancel") and get_tree().paused:
+		hide_menu()
+	
