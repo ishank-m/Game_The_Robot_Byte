447dbd4805f63ca6ad3bd1c12788304399176454
Aqdas Rafat Khan <chessaqdas03@gmail.com>
2024-08-19 21:28:38 +0530

    soldier added

---
+++ MyGame/scenes/characters/NPCs/soldier.gd
@@ -0,0 +1,20 @@
+extends CharacterBody2D
+
+const speed = 30
+
+@onready var destination = get_node("destination")
+@onready var nav_agent := $NavigationAgent2D as NavigationAgent2D
+
+func _ready():
+	makepath()
+
+func _physics_process(_delta):
+	var dir = to_local(nav_agent.get_next_path_position()).normalized()
+	velocity = dir * speed
+	move_and_slide()
+
+func makepath():
+	nav_agent.target_position = destination.position.y
+
+func _on_path_timer_timeout():
+	makepath()
