497a3d49fe485ba1d42c69b27de5fe22548d6d0e
Ishank <ishankmishra19@gmail.com>
2024-08-20 23:19:49 +0530

    some changes

--- MyGame/scenes/world/worldScene_3.gd
+++ MyGame/scenes/world/worldScene_3.gd
@@ -1,21 +1,21 @@
 extends Node2D
 @onready var transition = $TransitionScene
 @onready var path_1 = $Path_1/PathFollow2D
-@onready var path_2 = $Path_2/PathFollow2D
 @onready var MCPath = $PathMC/PathFollow2D
 var anim_playing =false
 var scene = 1
 var speed: int = 50
 var dialog = false
+var is_dialog_playing = false
 
 func _ready():
 	$Player.visible = false
-	$Path_2.visible = false
 	$ShopInterface.hide_shop()
 	MusicPlayer.stop()
 	transition.fade_out()
 	transition.connect("fade_in_done", _on_fade_in_done)
 	transition.connect("fade_out_done", _on_fade_out_done)
+	Dialogic.signal_event.connect(_on_dialogic_signal)
 func _process(delta):
 	path_1.progress += speed*delta
 	MCPath.progress += speed*delta
@@ -24,17 +24,17 @@ func _process(delta):
 		$PathMC/PathFollow2D/MC.play("up")
 		anim_playing = true
 	if MCPath.progress_ratio == 1:
-		$PathMC/PathFollow2D/MC.play("down")
-		$PathMC/PathFollow2D/MC.stop()
-	if path_1.progress_ratio == 1 and not dialog:
+		$PathMC/PathFollow2D/MC.visible == false
+		#$Player.set_position(MCPath.position)
+	if path_1.progress_ratio > 0.46 and path_1.progress_ratio < 0.463 and not is_dialog_playing:
+		speed = 0
 		$Path_1/PathFollow2D/King.stop()
-		dialog = true
-		
-	if dialog and path_1.progress_ratio == 1 and MCPath.progress_ratio == 1:
 		Dialogic.start("1worlscene3")
-		dialog = false
+		is_dialog_playing = true
 
-
+func _on_dialogic_signal(argument):
+	if argument == "done1":
+		speed = 50
 func _on_fade_out_done():
 	GameState.game_state = "play"
 
