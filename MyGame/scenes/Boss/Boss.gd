7512eb74956032222529bed9144cac5e808242cd
Aqdas Rafat Khan <chessaqdas03@gmail.com>
2024-08-21 13:21:12 +0530

    boss done(change its health)

--- MyGame/scenes/Boss/Boss.gd
+++ MyGame/scenes/Boss/Boss.gd
@@ -2,38 +2,58 @@ extends CharacterBody2D
 
 const speed = 80
 
+@onready var player = get_parent().get_node("Player")
 @onready var anim = $Boss_sprite
 
+var health = 10
 var attack = false
+var died = false
 
-func _physics_process(_delta):
-	var direction = Vector2()
-	if not attack:
-		if Input.is_action_just_pressed("attack"):
-			attack = true
-			anim.play("attack")
-		elif Input.is_action_pressed("ui_up"):
-			direction.y -= 1
-		elif Input.is_action_pressed("ui_down"):
-			direction.y += 1
-		elif Input.is_action_pressed("ui_left"):
-			direction.x -= 1
-		elif Input.is_action_pressed("ui_right"):
-			direction.x += 1
-		elif direction.length() > 0:
-			direction = direction.normalized()
-		velocity = direction*speed
-		update_anim(direction)
-		move_and_slide()
+func _ready():
+	$attackarea.add_to_group("boss")
+	$attackarea/CollisionShape2D.disabled = true
+
+func _physics_process(delta):
+	if not died and is_instance_valid(player):
+		if health <= 0:
+			died = true
+			anim.play("dead")
+		var y_diff = position.y - player.position.y
+		var direction = Vector2.ZERO
+		if not attack:
+			if -5 < y_diff and y_diff < 5:
+				attack = true
+				anim.stop()
+				$Timer.start()
+			elif y_diff < 0:
+				direction.y += 1
+			elif y_diff > 0:
+				direction.y -= 1
+			velocity = direction * speed
+			update_anim(direction)
+			move_and_collide(velocity*delta)
 
 func update_anim(direction):
-	if direction == Vector2(0,0) and not attack:
-		anim.stop()
-		return
-	elif not attack:
+	if not attack:
 		anim.play("walk")
-
 
 func _on_boss_sprite_animation_finished():
 	if anim.animation == "attack":
 		attack = false
+	if anim.animation == "dead":
+		anim.play("died")
+
+func _on_attackarea_2_area_entered(area):
+	if area.is_in_group("Player"):
+		GameState.player_health -= 10
+
+func _on_hurtbox_area_entered(area):
+	if area.is_in_group("player_attack"):
+		health -= GameState.sword_damage
+
+func _on_timer_timeout():
+	if not died:
+		anim.play("attack")
+		$attackarea/CollisionShape2D.disabled = false
+		await anim.animation_finished
+		$attackarea/CollisionShape2D.disabled = true
